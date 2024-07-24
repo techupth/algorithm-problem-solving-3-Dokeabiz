3bcff1e9755a671037e649fd7e392b7bcbb993f6
tle <tlemakuz@gmail.com>
2024-07-24 15:10:31 +0700

    result

--- ex-1.js
+++ ex-1.js
@@ -1,5 +1,25 @@
 function sortCustomerName(customers) {
-  // เริ่มเขียนโค้ดตรงนี้จ้า
+ const result = "ตอนนี้ผมยังปวดหัวกับโปรเจคกลุ่มที่ทำวันนี้อยู่ยังคิดไรไม่ออก ก๊อปโจทย์"
+                      "ไว้เสาทิตย์จะไปนั่งทำความเข้าใจน่ะครับ ตอนนี้ในหัวโล่งไปหมด";
 }
+const customers = ["Somchai", "Arthit", "Suchada", "Napasorn", "Pimchanok"]
+sortCustomerName(customers);
+ // customers.sort((a, b) => a.localeCompare(b));
+  // console.log(customers)
+
+  // เรามี Array `customers` ซึ่งบรรจุรายชื่อสมาชิก ให้ใช้ Bubble Sort Algorithm เพื่อเรียงลำดับสมาชิกตามลำดับตัวอักษร 
+
+  // เช่น ถ้า `customers = ["Somchai", "Arthit", "Suchada", "Napasorn", "Pimchanok"]` เมื่อ Algorithm นี้ทำงานจะแสดงผลลัพธ์ออกมาบนหน้าจอเป็น `[ "Arthit", "Napasorn", "Pimchanok", "Somchai", "Suchada"]`
+  
+  // ```jsx
+  // function sortCustomerName(customers) {
+  //   // เริ่มเขียนโค้ดตรงนี้จ้า
+  // }
+  
+  // // ตอบคำถามตรงนี้จ้า
+  // ```
+  
+  // ยังจำได้ไหมว่า Bubble Sort Algorithm มี Big O เป็นเท่าไร เพราะอะไร ให้เขียนอธิบายใน comment แนบมาข้างใต้โค้ดตรงส่วนที่ระบุว่า **“ตอบคำถามตรงนี้จ้า”**
+
 
 // ตอบคำถามตรงนี้จ้า
--- ex-2.js
+++ ex-2.js
@@ -1,3 +1,35 @@
 function sortStudentsByScore(students) {
-  // เริ่มเขียนโค้ดตรงนี้จ้า
+  
 }
+
+const students = [
+  { id: "S001", name: "Anan", score: 85 },
+  { id: "S002", name: "Boon", score: 92 },
+  { id: "S003", name: "Chai", score: 78 },
+  { id: "S004", name: "Dao", score: 95 },
+  { id: "S005", name: "Ek", score: 88 },
+  { id: "S006", name: "Fah", score: 80 }
+];
+sortStudentsByScore(students)
+
+// students.sort((a, b) => b.score - a.score);
+// console.log(students)
+// เรามี Array students ซึ่งบรรจุข้อมูลนักเรียน ให้เขียน Algorithm เพื่อเรียงลำดับนักเรียนตามคะแนนสอบ โดยเรียงจากคนที่สอบได้คะแนนสูงสุดไปยังคนที่สอบได้คะแนนต่ำสุด เช่น
+// [
+//     { id: "S001", name: "Anan", score: 85 },
+//     { id: "S002", name: "Boon", score: 92 },
+//     { id: "S003", name: "Chai", score: 78 },
+//     { id: "S004", name: "Dao", score: 95 },
+//     { id: "S005", name: "Ek", score: 88 },
+//     { id: "S006", name: "Fah", score: 80 }
+// ];
+// ​
+// เมื่อ Algorithm นี้ทำงานจะเห็นผลลัพธ์ดังนี้
+// [
+//     { id: "S004", name: "Dao", score: 95 },
+//     { id: "S002", name: "Boon", score: 92 },
+//     { id: "S005", name: "Ek", score: 88 },
+//     { id: "S001", name: "Anan", score: 85 },
+//     { id: "S006", name: "Fah", score: 80 },
+//     { id: "S003", name: "Chai", score: 78 }
+// ]
